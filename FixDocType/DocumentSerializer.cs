91f13fe5ecbc35133f5e41c85b6f414f595c0952
jalalx <sjamini@gmail.com>
2018-03-05 16:12:07 +0330

    Add project files.

---
+++ FixDocType/DocumentSerializer.cs
@@ -0,0 +1,83 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace FixDocType
+{
+    public class DocumentSerializer : IDisposable
+    {
+        private const string UniquePostfix = "STEWIESAYSWHATTHEDEUCE";
+        private readonly MemoryStream _docStream = null;
+        private readonly WordprocessingDocument _wordDocument = null;
+        private string _documentText = null;
+
+        public DocumentSerializer(byte[] document)
+        {
+            _docStream = new MemoryStream(document, true);
+            _wordDocument = WordprocessingDocument.Open(_docStream, true);
+            using (var reader = new StreamReader(_wordDocument.MainDocumentPart.GetStream()))
+            {
+                _documentText = reader.ReadToEnd();
+            }
+        }
+
+        const string WrongPattern = @"DOCPROPERTY CrmProperty(?<junk1>.*?>)(?<index>[0-9]{1,3}\s)(?<junk2>.*?)(?<ending>\\\* CHARFORMAT\s+\\\*\s+MERGEFORMAT)";
+        public void Fix()
+        {
+            var match = Regex.Match(_documentText, WrongPattern);
+            while (match.Success)
+            {
+                var junk1 = match.Groups["junk1"];
+                var index = match.Groups["index"];
+                var junk2 = match.Groups["junk2"];
+                var ending = match.Groups["ending"];
+
+                var fixedValue = "DOCPROPERTY CRMPROPERTY" + index + ending;
+                _documentText = _documentText.Replace(match.Value, fixedValue);
+
+                match = Regex.Match(_documentText, WrongPattern);
+            }
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                if (_wordDocument != null)
+                {
+                    _wordDocument.Dispose();
+                    _documentText = null;
+                }
+
+                if (_docStream != null)
+                {
+                    _docStream.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                Program.Error(ex.ToString());
+            }
+        }
+
+        //public void Replace(string find, string replace)
+        //{
+        //    var regexPatternFormat = string.Format("{{0}}(?!{0}|\\d)", UniquePostfix);
+        //    var pattern = string.Format(regexPatternFormat, find);
+        //    var newTempKey = replace + UniquePostfix;
+        //    _documentText = Regex.Replace(_documentText, pattern, newTempKey);
+        //}
+
+        public byte[] ToBytes()
+        {
+            _documentText = _documentText.Replace(UniquePostfix, string.Empty);
+            using (var writer = new StreamWriter(_wordDocument.MainDocumentPart.GetStream(FileMode.Create)))
+            {
+                writer.Write(_documentText);
+            }
+
+            return _docStream.ToArray();
+        }
+    }
+}
---
+++ FixDocType/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixDocType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                Error("Enter MS Word Document file path.");
+                return;
+            }
+
+            var fileInfo = new FileInfo(args[0]);
+            if (fileInfo.Extension.ToLower() != ".docx")
+            {
+                Error("Only MS Word document with .docx extension are supported.");
+                return;
+            }
+
+            var content = File.ReadAllBytes(fileInfo.FullName);
+            var serializer = new DocumentSerializer(content);
+            serializer.Fix();
+
+            var newFileName = fileInfo.FullName.Replace(fileInfo.Extension, "_fixed" + fileInfo.Extension);
+
+            File.WriteAllBytes(newFileName, serializer.ToBytes());
+            Console.WriteLine("MS Word file '{0}' fixed successfully.", newFileName);
+            Console.ReadKey();
+        }
+
+        public static void Error(string error, params object[] args)
+        {
+            var previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error, args);
+            Console.ForegroundColor = previousColor;
+        }
+    }
+}
